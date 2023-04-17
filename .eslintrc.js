d9679b8376b204405738683d531839d2cddbf5ca
Iamck <thanhson_it201@yahoo.com>
2023-04-17 14:51:10 +0700

    Linter & Formatter (#5)
    * Add dependabot
    
    * Add base config for linter
    
    * Lint fix
    
    * Integrate sonarjs plugin with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'sonarjs'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:sonarjs/recommended',
   ],
   root: true,
   env: {
@@ -16,9 +20,67 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+      {
+        ignoredNodes: ['FunctionExpression', 'PropertyDefinition'],
+        SwitchCase: 1,
+      },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 1,
+    '@typescript-eslint/no-inferrable-types': [
+      'warn',
+      {
+        ignoreParameters: true,
+      },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'eol-last': ['error', 'always'],
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        'newlines-between': 'always',
+      },
+    ],
+    indent: 'off',
+    'linebreak-style': ['error', 'unix'],
+    'no-case-declarations': 0,
+    'no-console': 0,
+    'no-multi-spaces': 'error',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 1,
+      },
+    ],
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
   },
-};
+  overrides: [
+    {
+      files: ['test/**/*.ts', '**/*.spec.ts'],
+      rules: {
+        'sonarjs/no-duplicate-string': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+    'import/ignore': ['node_modules'],
+  },
+}
