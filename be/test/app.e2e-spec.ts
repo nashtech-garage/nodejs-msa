1bfa07c460638f05d17548a19e1c763def8b910c
Iamck <thanhson_it201@yahoo.com>
2023-04-20 11:05:04 +0700

    Refactor monorepo (#20)
    * yarn stage
    
    * Add yarn plugin
    
    * Move nestjs to be workspace
    
    * Update CI workflow

--- be/test/app.e2e-spec.ts
+++ be/test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { INestApplication } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import * as request from 'supertest'
 
-import { AppModule } from './../src/app.module'
+import { AppModule } from '../src/app.module'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
