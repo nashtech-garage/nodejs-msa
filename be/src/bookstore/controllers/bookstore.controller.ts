5c2cf7be88159ac636c8095d41a552e2d941fd6d
Iamck <thanhson_it201@yahoo.com>
2023-04-24 11:23:46 +0700

    Update bookstore API response description

--- be/src/bookstore/controllers/bookstore.controller.ts
+++ be/src/bookstore/controllers/bookstore.controller.ts
@@ -8,14 +8,13 @@ import { ApiTag } from '@/shared/constants'
 
 @Controller('bookstore')
 @ApiTags(ApiTag.Bookstore)
-@ApiResponse({ status: HttpStatus.OK, description: 'OK' })
 export class BookstoreController {
   constructor(private readonly bookstoreService: BookstoreService) {}
 
   @Post()
   @ApiOperation({ summary: 'Create new bookstore' })
   @HttpCode(HttpStatus.CREATED)
-  @ApiResponse({ status: HttpStatus.CREATED, description: 'CREATED' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'This action adds a new bookstore' })
   async create(@Body() createBookstoreDto: CreateBookstoreDto) {
     const result = await this.bookstoreService.create(createBookstoreDto)
     return { message: 'This action adds a new bookstore', result }
@@ -23,6 +22,7 @@ export class BookstoreController {
 
   @Get()
   @ApiOperation({ summary: 'Get all bookstores' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'This action returns all bookstore' })
   async findAll() {
     const result = await this.bookstoreService.findAll()
     return { message: 'This action returns all bookstore', result }
@@ -31,6 +31,7 @@ export class BookstoreController {
   @Get(':id')
   @ApiOperation({ summary: 'Get bookstore' })
   @ApiParam({ name: 'id', example: uuidV4() })
+  @ApiResponse({ status: HttpStatus.OK, description: 'This action returns a bookstore' })
   async findOne(@Param('id') id: string) {
     const result = await this.bookstoreService.findOne(id)
     return { message: `This action returns a #${id} bookstore`, result }
@@ -39,6 +40,7 @@ export class BookstoreController {
   @Put(':id')
   @ApiOperation({ summary: 'Update bookstore' })
   @ApiParam({ name: 'id', example: uuidV4() })
+  @ApiResponse({ status: HttpStatus.OK, description: 'This action updates a bookstore' })
   async update(@Param('id') id: string, @Body() updateBookstoreDto: UpdateBookstoreDto) {
     const result = await this.bookstoreService.update(id, updateBookstoreDto)
     return { message: `This action updates a #${id} bookstore`, result }
@@ -48,7 +50,7 @@ export class BookstoreController {
   @ApiOperation({ summary: 'Delete bookstore' })
   @ApiParam({ name: 'id', example: uuidV4() })
   @HttpCode(HttpStatus.NO_CONTENT)
-  @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'NO_CONTENT' })
+  @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'This action removes a bookstore' })
   async remove(@Param('id') id: string) {
     const result = await this.bookstoreService.remove(id)
     return { message: `This action removes a #${id} bookstore`, result }
