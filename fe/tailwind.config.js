86f0ba2e489cdef38c701d8cc597c6cf37c2a2ab
Iamck <thanhson_it201@yahoo.com>
2023-04-26 16:28:00 +0700

    Config network policy for fe & be

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
