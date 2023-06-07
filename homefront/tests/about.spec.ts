fed60128c1bcb6a005e8489b7231d53cc0efe03a
Hung Nguyen <nxhung118@gmail.com>
2023-06-07 09:57:58 +0700

    change request comment

--- homefront/tests/about.spec.ts
+++ homefront/tests/about.spec.ts
@@ -1,10 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('check about us page', async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('http://localhost:3000/');
-
-  await page.click('text=About Us');
+  await page.goto('http://localhost:3000/about');
 
   await expect(page).toHaveURL('http://localhost:3000/about');
 
