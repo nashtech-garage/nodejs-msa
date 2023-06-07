b9502950c135be3199a5fc6944bc9c71beae1a38
Hung Nguyen <nxhung118@gmail.com>
2023-06-07 09:57:58 +0700

    change request comment

--- homefront/tests/home.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('check homepage', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-
-  await expect(page.locator('h1')).toContainText('HomePage');
-});
