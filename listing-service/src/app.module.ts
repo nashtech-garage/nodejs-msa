5b91c9238fbb6144574ce04b14213fa5ee894ebd
Thanh Pham <pano.tjoan@yahoo.com>
2023-06-09 00:11:30 -0700

    refactor(listing): update name

--- listing-service/src/app.module.ts
+++ listing-service/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prismas/prisma.module';
-import { ListingModule } from './listings/listing.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ListingModule } from './listing/listing.module';
 import { CategoryModule } from './categories/categories.module';
 
 @Module({
