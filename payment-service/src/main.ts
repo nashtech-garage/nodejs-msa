9168a093dcde29a3e07cbf5925dbd3b8b5a6d423
nvhai248 <nvhai2408@gmail.com>
2024-08-27 17:57:17 +0700

    refactor(bff, payment): using env in docker compose

--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PAYMENT_SERVICE_PORT || 8003;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  await app.listen(PORT);
+  console.log('Payment Service is listening on port ', PORT);
 }
 bootstrap();
