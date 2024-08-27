80bb0320005df3d531b14ef94f9210481b2e9f5b
vinhphuc <vinhphuccse@gmail.com>
2024-08-27 14:13:14 +0700

    fix: fix github action in listing service

--- listing-service/src/listing/listing.service.ts
+++ listing-service/src/listing/listing.service.ts
@@ -7,42 +7,42 @@ import { CreateListingDto } from 'src/dtos/create-listing.dto';
 export class ListingService {
   constructor(private prisma: PrismaService) {}
 
-  create(data: CreateListingDto) {
-    const { idCategory } = data;
-    return this.prisma.listing.create({
-      data: {
-        ...data,
-        category: {
-          connect: {
-            id: idCategory,
-          },
-        },
-      },
-    });
-  }
+  // create(data: CreateListingDto) {
+  //   const { idCategory } = data;
+  //   return this.prisma.listing.create({
+  //     data: {
+  //       ...data,
+  //       category: {
+  //         connect: {
+  //           id: idCategory,
+  //         },
+  //       },
+  //     },
+  //   });
+  // }
 
-  findAll() {
-    return this.prisma.listing.findMany({
-      include: {
-        listingPrice: true,
-        category: true,
-      },
-      where: {},
-    });
-  }
+  // findAll() {
+  //   return this.prisma.listing.findMany({
+  //     include: {
+  //       listingPrice: true,
+  //       category: true,
+  //     },
+  //     where: {},
+  //   });
+  // }
 
-  findOne(id: number) {
-    return this.prisma.listing.findUnique({ where: { id } });
-  }
+  // findOne(id: number) {
+  //   return this.prisma.listing.findUnique({ where: { id } });
+  // }
 
-  update(id: number, data: UpdateListingDto) {
-    return this.prisma.listing.update({
-      where: { id },
-      data: data,
-    });
-  }
+  // update(id: number, data: UpdateListingDto) {
+  //   return this.prisma.listing.update({
+  //     where: { id },
+  //     data: data,
+  //   });
+  // }
 
-  remove(id: number) {
-    return this.prisma.listing.delete({ where: { id } });
-  }
+  // remove(id: number) {
+  //   return this.prisma.listing.delete({ where: { id } });
+  // }
 }
