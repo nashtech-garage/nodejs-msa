ec8c0e80e78548261c16fdbb97d4813712f84ce1
Tri Do Minh <mitidevus@gmail.com>
2024-08-28 09:16:06 +0700

    chore: get port from env

--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(process.env.PAYMENT_SERVICE_PORT);
 }
 bootstrap();
