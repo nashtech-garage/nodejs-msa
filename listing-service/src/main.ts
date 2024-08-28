70f8ca010e6dcccc0f1c9db914c3ce1c2cca58fb
Tri Do Minh <mitidevus@gmail.com>
2024-08-28 09:16:06 +0700

    chore: get port from env

--- listing-service/src/main.ts
+++ listing-service/src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3003);
+  await app.listen(process.env.LISTING_SERVICE_PORT);
 }
 bootstrap();
