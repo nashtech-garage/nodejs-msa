9c2f22b36e72ce8a7fca52665d4f39b6c60ac888
Em Ha Tuan <eht42@open.ac.uk>
2024-09-04 15:59:27 +0700

    Mobile - Authentication screens

---
+++ mobile/merchant_app/lib/src/screens/welcome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome screen'),
+      ),
+    );
+  }
+}
