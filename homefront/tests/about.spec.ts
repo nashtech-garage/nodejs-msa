4b14535b0d09bb29c09d464c353f210e6498f79f
Tuong Le <tuong.levan@nashtechglobal.com>
2024-09-06 10:12:54 +0700

    Restructure Mono repo NestJS to avoid duplication code and simple development. - Restructure backend with nestjs to use same version of nestjs. - Restructure frontend app to use nextjs into folder `frontend` - Add `.nvmrc` node version to all applications both backend and frontend - Move `deployment` and `terraform` into `infrastructure` folder - Move `merchant_app` into `mobile` Restructure Mono repo NestJS to avoid duplication code and simple development. - Restructure backend with nestjs to use same version of nestjs. - Restructure frontend app to use nextjs into folder `frontend` - Add `.nvmrc` node version to all applications both backend and frontend - Add backend development into `docker-compose.yml` - Remove old services are not correctly with mono repo architecture.

--- homefront/tests/about.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('check about us page', async ({ page }) => {
-  await page.goto('http://localhost:3000/about');
-
-  await expect(page).toHaveURL('http://localhost:3000/about');
-
-  await expect(page.locator('h1')).toContainText('About us');
-});
